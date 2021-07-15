8e95ba37b31eb87ec000088ec1b26ce5a42bdd2b
dlalstj0213 <dlalstj0213@gmail.com>
2021-07-15 16:39:32 +0900

    remove deprecated code

--- 2_spring_boot/hello/src/main/java/com/example/hello/dto/PutRequest.java
+++ 2_spring_boot/hello/src/main/java/com/example/hello/dto/PutRequest.java
@@ -10,8 +10,11 @@ import java.util.List;
  * @JsonProperty 와 @JsonNaming 의 차이점
  * - @JsonProperty는 한가지 변수에 대해서 이름을 지어주는 어노테이션인 반면에,
  * - @JsonNaming은 일괄적으로 모든 JSON 룰을 정해줄 수 있다.
+ *
+ * 주의 : deprecated PropertyNamingStrategy by ~2.12
+ * 해결방법 : Use PropertyNamingStrategies
  */
-@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PutRequest {
     private String name;
     private int age;
